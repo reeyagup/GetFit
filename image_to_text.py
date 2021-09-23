68535a044d606391f2adf741d147157b2783bb0b
Reeya <rgup@ufl.edu>
2021-09-23 18:15:33 -0400

    test

--- image_to_text.py
+++ image_to_text.py
@@ -3,8 +3,22 @@ import PIL
 from os import system
 import re
 system("tesseract -l")
-letters = (pytesseract.image_to_string(r'../GetFit/workout_routine.jpeg'))
 
-text_words = re.findall(r'\w+', letters)
+
+class workout:
+      def __init__(self, reps, exercise_name):
+        self.reps = reps    
+        self.exercise_name = exercise_name
+
+
+def compile_text_to_workouts(text):
+    for word in text:
+        if word.isDigit():
+            reps = text[word]
+            new_workout = workout(reps)
+        else:
+            print("")
+            
+
 
 
