9eccfd10a2306e59dd79fcdc75baa467d76e2207
Reeya <rgup@ufl.edu>
2021-09-23 19:17:12 -0400

    getting text to print

--- image_to_text.py
+++ image_to_text.py
@@ -5,22 +5,37 @@ import re
 system("tesseract -l")
 
 class workout:
-      def __init__(self, reps, exercise_name):
-        self.reps = reps    
-        self.exercise_name = exercise_name
+    reps = 0
+    exercise_name = ""
 
 
 def compile_text_to_workouts(text):
     workouts = []
     num = 0
+
+   
     for word in text:
+        new_workout = workout()
         if word.isdigit():
-            new_workout = (word, text[num+1])
-            workouts.append(new_workout)
+            new_workout.reps = word
             num+=1
+        while num < len(text) and not text[num].isdigit() :
+            new_workout.exercise_name +=  " " + str(text[num]) 
+            num +=1
+        workouts.append(new_workout)
     
+    num = 0
     for x in workouts:
-        print (x)
+        if(x.reps == 0 or x.exercise_name == ""):
+           print("zero")
+           workouts.pop(num)
+        num +=1
+    
+    for y in workouts:
+        print(y.reps ," , ", str(y.exercise_name))
+
+
+        
 
 letters = (pytesseract.image_to_string(r'../GetFit/workout_routine1.png'))
 text_words = re.findall(r'\w+', letters)
